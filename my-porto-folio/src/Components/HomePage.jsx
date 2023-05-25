9ac1eb0e4fcc33e46823ef5f899063746d8e60ec
WTaroki <william.tanjaya@binus.ac.id>
2023-05-26 01:21:34 +0700

    Change Name

--- my-porto-folio/src/Components/HomePage.jsx
+++ my-porto-folio/src/Components/HomePage.jsx
@@ -9,7 +9,7 @@ const HomeContent = () => {
               <div className="max-w-[650px] mx-auto flex flex-col justify-center">
                     <p className='HP:text-xl text-gray-800'><br />―――― <span className='font-poppins text-xl'>Welcome</span> </p>
                     <h3 className=' text-1xl mt-2 text-gray-600 font-bold font-play md:text-2xl md:py-2'><Typed strings={['Bonjour','Halo','Hello','Hallo','Aloha']} loop typeSpeed={120} backSpeed={120} className='font-caveat font-bold text-gray-800 text-3xl'/>, I'm</h3>
-                    <h2 className=" text-2xl md:text-6xl font-bold md:py-2 HP:text-4xl text text-sky-900 mt-2">William Tanjaya</h2>
+                    <h2 className=" text-2xl md:text-6xl font-bold md:py-2 HP:text-4xl text text-sky-900 mt-2">Quite Taroki</h2>
                     <h3 className=' text-md text-gray-700 font-bold font-play md:text-md md:py-2 mt-2 md:mt-0'>Web Developer | Punya Toko | Mahasiswa</h3>
                     <p className='font-poppins mt-6 max-w-[500px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <p className="">
