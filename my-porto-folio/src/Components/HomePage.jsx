c24d58c6b26052ef8a11a06570d47c8f91530ae8
WTaroki <william.tanjaya@binus.ac.id>
2023-05-26 01:23:25 +0700

    Change Skills

--- my-porto-folio/src/Components/HomePage.jsx
+++ my-porto-folio/src/Components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomeContent = () => {
                     <p className='HP:text-xl text-gray-800'><br />―――― <span className='font-poppins text-xl'>Welcome</span> </p>
                     <h3 className=' text-1xl mt-2 text-gray-600 font-bold font-play md:text-2xl md:py-2'><Typed strings={['Bonjour','Halo','Hello','Hallo','Aloha']} loop typeSpeed={120} backSpeed={120} className='font-caveat font-bold text-gray-800 text-3xl'/>, I'm</h3>
                     <h2 className=" text-2xl md:text-6xl font-bold md:py-2 HP:text-4xl text text-sky-900 mt-2">Quite Taroki</h2>
-                    <h3 className=' text-md text-gray-700 font-bold font-play md:text-md md:py-2 mt-2 md:mt-0'>Web Developer | Punya Toko | Mahasiswa</h3>
+                    <h3 className=' text-md text-gray-700 font-bold font-play md:text-md md:py-2 mt-2 md:mt-0'>Web Developer | Punya Toko | Kerok Expert</h3>
                     <p className='font-poppins mt-6 max-w-[500px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <p className="">
                         <button className="p-2 pl-4 pr-4 border hover:border-gray-950 hover:scale-110 text-xl bg-gray-800 text-white hover:text-opacity-75 transition-all ease-linear duration-250 rounded-md mt-6">Learn More</button>
